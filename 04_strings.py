9202a5d7ea6c7db4749a4856d4d3bd360e1d157d
RishabhVachheta <rishabhvachheta525@gmail.com>
2024-01-09 20:21:17 +0530

    first commit

---
+++ 04_strings.py
@@ -0,0 +1,12 @@
+
+# b= "rishabh"
+# print(b)
+# print(type(b))
+
+greeting= "Good Morning,"
+name="Rishabh is a Good boy"
+print(greeting + name)
+print(name[0])
+print(len(name))
+print(name.endswith("boy"))
+print(name.count("a"))
